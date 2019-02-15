177961f545c33701cb73fb627608e92205d1dcdc
Shashank J <shashankreddy231@gmail.com>
2019-02-15 07:20:32 -0800

    Update wireless.py
    Finding upload and download rates.

--- wireless.py
+++ wireless.py
@@ -1,8 +1,24 @@
 import wmi
+import speedtest
+
 c = wmi.WMI ()
+
+source = ""
  
 for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
-  print (interface.Description)
-  for ip_address in interface.IPAddress:
-    print (ip_address)
-  print("\n")
+  if "ireless" in interface.Description:
+  	source = str(interface.IPAddress[0])
+
+
+s = speedtest.Speedtest(source_address=source)
+s.get_best_server()
+print(s.download())
+print(s.upload())
+
+
+# for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
+#   print (interface.Description)
+#   print (type(interface.Description))
+#   for ip_address in interface.IPAddress:
+#     print (ip_address)
+#     source = str(ip_address)
